da0f6899215b805f8d071917f3b3adc9e3866afe
rg9400 <39887349+rg9400@users.noreply.github.com>
2020-10-27 22:11:23 -0500

    code cleanup

--- trakt_seasons.py
+++ trakt_seasons.py
@@ -137,11 +137,15 @@ def main():
     deduped_list = set(process_list)      
 
     if args.command == 'reset':
+        reset_show_counter = 0
+        reset_season_counter = 0
         log.info('Begining reset process')
         reset_title_locking_indicator = int('title' not in args.unlock)
         reset_summary_locking_indicator = int('summary' not in args.unlock)
         for show in deduped_list:
+            reset_show_counter += 1
             for season in show.seasons():
+                reset_season_counter += 1
                 old_season_title = season.title
                 old_season_summary = season.summary
                 edit = {}
@@ -171,9 +175,14 @@ def main():
                     """
                     .format(show.title, season.seasonNumber, old_season_summary, season.reload().summary, bool(reset_summary_locking_indicator)))
         log.info("Reset process finished")
+        log.info("Reset {} seasons across {} shows".format(reset_season_counter, reset_show_counter))
 
     if args.command == 'pull':
         log.info('Starting pull process')
+        show_counter = 0
+        season_counter = 0
+        new_title_counter = 0
+        new_summary_counter = 0
         pull_successful_title_locking_indicator = int('successful_title' not in args.unlock)
         pull_failed_title_locking_indicator = int('failed_title' not in args.unlock)
         pull_succesful_summary_locking_indicator = int('successful_summary' not in args.unlock)
@@ -183,17 +192,18 @@ def main():
         else:
             log.info("Force refresh disabled. Will filter out locked items")
         for show in deduped_list:
+            season_list = list(filter(lambda x: (x.seasonNumber != 0), show.seasons()))
             if not args.force:
                 if 'title' in args.data:
-                    all_titles_locked = all("title" in (field.name for field in season.fields) for season in show.seasons() if season.seasonNumber != 0)
+                    all_titles_locked = all("title" in (field.name for field in season.fields) for season in season_list)
                 else:
                     all_titles_locked = True
                 if 'summary' in args.data:
-                    all_summaries_locked = all("summary" in (field.name for field in season.fields) for season in show.seasons() if season.seasonNumber != 0)
+                    all_summaries_locked = all("summary" in (field.name for field in season.fields) for season in season_list)
                 else:
                     all_summaries_locked = True
                 if all_titles_locked and all_summaries_locked:
-                    log.info('Skipping show {} because force refresh is disabled and all requested items are locked'.format(show.title))
+                    log.debug('Skipping show {} because force refresh is disabled and all requested items are locked'.format(show.title))
                     continue
             if "thetvdb" in show.guid:
                 id = int(re.search(r'thetvdb:\/\/(.*)\?', show.guid).group(1))
@@ -203,77 +213,82 @@ def main():
                 trakt_search_api = 'https://api.trakt.tv/search/tmdb/{}?type=show'.format(id)
             else: 
                 log.warning('Could not find a TVDB or TMDB ID for show {}'.format(show.title))
-                id = None
-                slug = None
-
-            if id:
+                continue
+                
+            try:
                 trakt_search = requests.get(trakt_search_api, headers=trakt_headers).json()
-                try:
-                    slug = trakt_search[0]['show']['ids']['slug']
-                except:
-                    log.warning('Could not find Trakt slug for show {} and guid {}'.format(show.title, show.guid))
-                    slug = None
-            else:
-                log.warning('No guid found for show {}, skipping'.format(show.title))
+                slug = trakt_search[0]['show']['ids']['slug']
+            except:
+                log.warning('Could not find Trakt slug for show {} and guid {}'.format(show.title, show.guid))
+                continue
 
-            if slug:
-                trakt_season_api = 'https://api.trakt.tv/shows/{}/seasons?extended=full'.format(slug)
-                trakt_seasons = requests.get(trakt_season_api, headers=trakt_headers).json()
-                for season in trakt_seasons:
-                    season_number = season['number']
-                    if season_number != 0:
-                        season_title = season['title']
-                        if season_title == 'Season {}'.format(season_number):
-                            season_title = None
-                        season_summary = season['overview']
-                        try:
-                            plex_season = show.season(season_number)
-                            if not args.force:
-                                locked_title = "title" in (field.name for field in plex_season.fields)
-                                locked_summary = "summary" in (field.name for field in plex_season.fields)
+            trakt_season_api = 'https://api.trakt.tv/shows/{}/seasons?extended=full'.format(slug)
+            try:
+              trakt_seasons = requests.get(trakt_season_api, headers=trakt_headers).json()
+            except:
+              log.warning("Trakt season page inaccessible, skipping show {}".format(show.title))
+              continue
+            show_counter += 1
+            for season in trakt_seasons:
+                season_number = season['number']
+                if season_number != 0:
+                    season_title = season['title']
+                    if season_title == 'Season {}'.format(season_number):
+                        season_title = None
+                    season_summary = season['overview']
+                    try:
+                        plex_season = show.season(season_number)
+                        if not args.force:
+                            locked_title = "title" in (field.name for field in plex_season.fields)
+                            locked_summary = "summary" in (field.name for field in plex_season.fields)
+                        else:
+                            locked_title = False
+                            locked_summary = False
+                        if not (locked_title and locked_summary):
+                            season_counter += 1
+                        old_season_title = plex_season.title
+                        old_season_summary = plex_season.summary
+                        edit = {}
+                        if 'title' in args.data and not locked_title:
+                            if season_title:
+                                edit['title.value'] = season_title
+                                edit['title.locked'] = pull_successful_title_locking_indicator
+                                log.debug("""
+                                Show: {}
+                                Season: {}
+                                Old Title: {}
+                                New Title: {}
+                                Locked: {}
+                                """
+                                .format(show.title, plex_season.seasonNumber, old_season_title, season_title, bool(pull_successful_title_locking_indicator)))
+                                new_title_counter += 1
                             else:
-                                locked_title = False
-                                locked_summary = False
-                            old_season_title = plex_season.title
-                            old_season_summary = plex_season.summary
-                            edit = {}
-                            if 'title' in args.data and not locked_title:
-                                if season_title:
-                                    edit['title.value'] = season_title
-                                    edit['title.locked'] = pull_successful_title_locking_indicator
-                                    log.info("""
-                                    Show: {}
-                                    Season: {}
-                                    Old Title: {}
-                                    New Title: {}
-                                    Locked: {}
-                                    """
-                                    .format(show.title, plex_season.seasonNumber, old_season_title, season_title, bool(pull_successful_title_locking_indicator)))
-                                else:
-                                    edit['title.value'] = old_season_title
-                                    edit['title.locked'] = pull_failed_title_locking_indicator
-                                    log.debug("{} Season {} - No title found on Trakt. Locked on Plex: {}". format(show.title, plex_season.seasonNumber, bool(pull_failed_title_locking_indicator)))
-                            if 'summary' in args.data and not locked_summary:
-                                if season_summary:
-                                    edit['summary.value'] = season_summary
-                                    edit['summary.locked'] = pull_succesful_summary_locking_indicator
-                                    log.info("""
-                                    Show: {}
-                                    Season: {}
-                                    Old Summary: {}
-                                    New Summary: {}
-                                    Locked: {}
-                                    """
-                                    .format(show.title, plex_season.seasonNumber, old_season_summary, season_summary, bool(pull_succesful_summary_locking_indicator)))
-                                else:
-                                    edit['summary.value'] = old_season_summary
-                                    edit['summary.locked'] = pull_failed_summary_locking_indicator
-                                    log.debug("{} Season {} - No summary found on Trakt. Locked on Plex: {}".format(show.title, plex_season.seasonNumber, bool(pull_failed_summary_locking_indicator)))
-                            plex_season.edit(**edit)
-                        except:
-                            log.warning("{} Season {} exists on Trakt but not in Plex".format(show.title, season_number))
+                                edit['title.value'] = old_season_title
+                                edit['title.locked'] = pull_failed_title_locking_indicator
+                                log.debug("{} Season {} - No title found on Trakt. Locked on Plex: {}". format(show.title, plex_season.seasonNumber, bool(pull_failed_title_locking_indicator)))
+                        if 'summary' in args.data and not locked_summary:
+                            if season_summary:
+                                edit['summary.value'] = season_summary
+                                edit['summary.locked'] = pull_succesful_summary_locking_indicator
+                                log.debug("""
+                                Show: {}
+                                Season: {}
+                                Old Summary: {}
+                                New Summary: {}
+                                Locked: {}
+                                """
+                                .format(show.title, plex_season.seasonNumber, old_season_summary, season_summary, bool(pull_succesful_summary_locking_indicator)))
+                                new_summary_counter += 1
+                            else:
+                                edit['summary.value'] = old_season_summary
+                                edit['summary.locked'] = pull_failed_summary_locking_indicator
+                                log.debug("{} Season {} - No summary found on Trakt. Locked on Plex: {}".format(show.title, plex_season.seasonNumber, bool(pull_failed_summary_locking_indicator)))
+                        plex_season.edit(**edit)
+                    except:
+                        log.warning("{} Season {} exists on Trakt but not in Plex".format(show.title, season_number))
             time.sleep(5)
         log.info("Pull process finished")
+        log.info("Processed {} shows across {} seasons. Found {} new titles and {} new summaries".format(show_counter, season_counter, new_title_counter, new_summary_counter))
 
 if __name__ == "__main__":
     main()
